b6c5096bf66b6e375e8c41d692975883204161de
Aisha Atif <143438686+FiendOfAshes@users.noreply.github.com>
2024-01-10 18:43:33 +0400

    Update add_binary.py
    modified comments

--- add_binary.py
+++ add_binary.py
@@ -6,24 +6,19 @@
 def decimal(n, exp=1):
     if n == 0:
         return n
-    remainder = n%10  # modulo 10 to store the last digit
-    remainder = remainder*exp  #multiply by exp to update
+    remainder = n%10  
+    remainder = remainder*exp 
     return remainder + decimal(n//10, exp*2)
-# add this
 
-# print(decimal(1101,))
 # Second step: converting decimal to binary
 def binary(x):
     if x == 0:
         return str(x)
     return x%2 + 10*int((binary(x//2)))
 
-# print(binary(13))
-
-# Finally, to add to n-bit numbers
+# Finally, to add two n-bit binary integers in arrays
 def add_binary(a, b):
     A = "".join([str(x) for x in a])
     B = "".join([str(x) for x in b])
     return binary(decimal(int(A)) + decimal(int(B)))
 
-print(add_binary([1, 0, 1], [1, 1, 0, 1]))
